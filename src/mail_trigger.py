c2a6accf9da82ce4a5eff437dfdef5522ecae58d
hyCheng <pete710592@gmail.com>
2018-12-14 19:09:24 +0100

    Add Publisher & Subscriber for topic /mail

--- src/mail_trigger.py
+++ src/mail_trigger.py
@@ -4,25 +4,33 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import *
 from linebot.exceptions import LineBotApiError
+from hbc_msgs.msg import MailStamped
 
 # Channel Access Token
 line_bot_api = LineBotApi('6CXOEtUWC0yteVKp84RJaUHuaJBf8i8VryR1mtT9gvqSuN2v5gaYl9EdUuYuUebPGMFCiQsWt3xKrLjtB6JlmQkaBagdDBBTsyi/8mL+TZlQQttuYdT5jCIRkVds4oyP+Vj2L9Ty9aYyN6wU8y7MaAdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('22ee390e67704d39b3ca17d0881f30de')
 # User ID
-to = "U61f94258daedd7a7e8c2b9519e64e13b"
+#to = "U61f94258daedd7a7e8c2b9519e64e13b"
 
 class MailTrigger(object):
 	def __init__(self):
-		self.deliver()
+		self.sub_mail = rospy.Subscriber('/mail', MailStamped, self.cbMail, queue_size=1)
 		
-	def deliver(self):
+	def cbMail(self, mail_msg):
+		mail_msg = mail_msg
+		ID = mail_msg.ID
+		mailNum = mail_msg.mailNum
+		self.deliver(ID, mailNum)
+		print("subscribe Successfully!")  #######################################
+		
+	def deliver(self, ID, mailNum):
 		message = TemplateSendMessage(
 			alt_text='Mail trigger',
 			template=ButtonsTemplate(
 				thumbnail_image_url='https://uwaterloo.ca/central-stores/sites/ca.central-stores/files/styles/body-500px-wide/public/uploads/images/little_man_holding_red_envelope.jpg',
 				title='Duckiebot #01',
-				text='You have 2 mails. \nWhat can I do for you?',
+				text='You have ' + mailNum + ' mails. \nWhat can I do for you?',
 				actions=[
 					PostbackTemplateAction(
 						label='drop it for me',	 # displayed on the screen
@@ -36,7 +44,7 @@ class MailTrigger(object):
 				]
 			)
 		)
-		line_bot_api.push_message(to, message)
+		line_bot_api.push_message(ID, message)
 
 if __name__ == "__main__":
 	rospy.init_node("mail_trigger",anonymous=False)
